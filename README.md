2ea862b01150f884a725b25e90b32c10e25fb1d4
kevin <kevin.graff1@gmail.com>
2024-06-19 23:45:50 +0200

    feat(init): add decodeToken.ts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jwt-crypto
 Encrypt and decrypt JSON Web Tokens (JWTs) securely. Utilizes strong encryption algorithms (AES, RSA, HMAC) for data confidentiality. Ideal for enhancing JWT security in authentication systems.
+
+Encrypt is asynchronous and decrypt is synchronous. This is because encryption is computationally expensive and should be done on the server side. Decryption is computationally cheap and can be done on the client side.
---
+++ src/decodeToken.ts
@@ -0,0 +1,64 @@
+interface DecodedToken {
+    header: object;
+    payload: object;
+}
+
+/**
+ * Decodes a Base64-URL encoded string.
+ * @param input The Base64-URL encoded string to decode.
+ */
+const base64UrlDecode = (input: string): string => {
+    input = input.replace(/-/g, '+').replace(/_/g, '/');
+    const padding = input.length % 4;
+    if (padding) {
+        input += '='.repeat(4 - padding);
+    }
+    return atob(input);
+};
+
+/**
+ * Verifies the HMAC SHA-256 signature for the provided data using the given key.
+ * @param key The secret key used for HMAC signing.
+ * @param data The data to be verified.
+ * @param expectedSignature The expected Base64-URL encoded HMAC SHA-256 signature.
+ * @returns true if the signature matches, false otherwise.
+ */
+const verifyHmacSha256Signature = async (key: string, data: string, expectedSignature: string): Promise<boolean> => {
+    const encoder = new TextEncoder();
+    const cryptoKey = await crypto.subtle.importKey("raw", encoder.encode(key), { hash: "SHA-256", name: "HMAC" }, false, ["verify"]);
+    const signature = Uint8Array.from(atob(expectedSignature), c => c.charCodeAt(0));
+
+    return crypto.subtle.verify("HMAC", cryptoKey, encoder.encode(data), signature);
+};
+
+/**
+ * Decodes the provided JWT token using the specified secret key.
+ * Verifies the signature and returns the decoded token if valid.
+ * @param token The JWT token string to decode.
+ * @param secretKey The secret key used for HMAC SHA-256 signing.
+ * @returns The decoded token as an object if valid, otherwise null.
+ */
+const decodeToken = async (token: string, secretKey: string): Promise<DecodedToken | null> => {
+    const parts = token.split('.');
+    if (parts.length !== 3) {
+        return null;
+    }
+
+    const [headerEncoded, payloadEncoded, signature] = parts;
+
+    const header = JSON.parse(base64UrlDecode(headerEncoded));
+    const payload = JSON.parse(base64UrlDecode(payloadEncoded));
+    const dataToVerify = `${headerEncoded}.${payloadEncoded}`;
+    const verifiedSignIn = await verifyHmacSha256Signature(secretKey, dataToVerify, signature);
+
+    if (!verifiedSignIn) {
+        return null;
+    }
+
+    return {
+        header,
+        payload
+    };
+};
+
+export default decodeToken;
