17133ffe12b0b6ad3cb7c12dcbe56627cb10225f
kevin <kevin.graff1@gmail.com>
2024-06-20 23:12:38 +0200

    feat(init): finish config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
+import { peerDependencies } from "./package.json";
+import path from 'path';
+
+const allDependencies = [...Object.keys(peerDependencies), ...Object.keys(peerDependencies)];
+const external = Array.from(new Set(allDependencies));
+
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'), // Assurez-vous d'ajuster ce chemin selon votre structure de projet
+        },
+    },
     build: {
-        outDir: 'dist',
         rollupOptions: {
-            input: 'src/main.ts'
-        }
-    }
+            input: {
+                main: 'src/main.ts',
+            },
+            external,
+        },
+    },
 });
